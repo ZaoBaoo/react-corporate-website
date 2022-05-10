3fa0a678bd7ccddcd9baf849c7673901d63dec08
ZaoBaoo <tampooox@gmail.com>
2022-05-10 22:51:23 +0600

    add /main/general

--- src/components/Push/Push.jsx
+++ src/components/Push/Push.jsx
@@ -4,37 +4,11 @@ import { Container } from "../Container";
 // Style
 import styles from "./Push.module.scss";
 
-const Push = () => {
+const Push = ({ children }) => {
   return (
     <div className={styles.pushBlock}>
       <Container>
-        <div className={styles.searchBlock}>
-          <div className={styles.search}>
-            <input type="search" spellCheck={false} placeholder="Поиск..." />
-
-            <div className={styles.dropdown}>
-              <input className={styles.dropBtn} type="text" readOnly={true} />
-
-              <div className={styles.dropdownContent}>
-                <div className={styles.textContent}>Фильтр:</div>
-                <dir className={styles.groupRadio}>
-                  <input type="radio" name="search" id="firstname" />
-                  <label htmlFor="firstname">Имя</label>
-                </dir>
-
-                <dir className={styles.groupRadio}>
-                  <input type="radio" name="search" id="lastname" />
-                  <label htmlFor="lastname">Фамиилия</label>
-                </dir>
-
-                <dir className={styles.groupRadio}>
-                  <input type="radio" name="search" id="job" />
-                  <label htmlFor="job">Отдел</label>
-                </dir>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className={styles.pushContent}>{children}</div>
       </Container>
     </div>
   );
--- src/components/layout/Main/Main.jsx
+++ src/components/layout/Main/Main.jsx
@@ -13,18 +13,98 @@ import { userDBAction } from "../../../store/slice/userDBSlice";
 // Components
 import { Header } from "../../Header";
 import { Push } from "../../Push";
+import { Search } from "../../Search";
 import { Container } from "../../Container";
 
+import miniUserIcon from "../../../img/miniUserIcon.png";
+import messageIcon from "../../../img/messageIcon.svg";
 // Style
 import styles from "./Main.module.scss";
 
 const Main = () => {
-  const { userData } = useSelector((state) => state.userDB);
-
   return (
     <>
       <Header />
-      <Push />
+      <Push>
+        <Search />
+      </Push>
+
+      <div className={styles.generalBlock}>
+        <Container>
+          <div className={styles.generalContent}>
+            <div className={styles.leftSide}>
+              <div className={styles.personBox}>
+                <div className={styles.infoUser}>
+                  <div className={styles.imgUser}>
+                    <img src={miniUserIcon} alt="" />
+                  </div>
+                  <div className={styles.infoName}>
+                    <span>Марина Путина</span>
+                    <div className={styles.infoDepartment}>СММ специалист</div>
+                  </div>
+                </div>
+                <img
+                  src={messageIcon}
+                  alt=""
+                  className={styles.messageUserIcon}
+                />
+              </div>
+
+              <div className={styles.personBox}>
+                <div className={styles.infoUser}>
+                  <div className={styles.imgUser}>
+                    <img src={miniUserIcon} alt="" />
+                  </div>
+                  <div className={styles.infoName}>
+                    <span>Марина Путина</span>
+                    <div className={styles.infoDepartment}>СММ специалист</div>
+                  </div>
+                </div>
+                <img
+                  src={messageIcon}
+                  alt=""
+                  className={styles.messageUserIcon}
+                />
+              </div>
+
+              <div className={styles.personBox}>
+                <div className={styles.infoUser}>
+                  <div className={styles.imgUser}>
+                    <img src={miniUserIcon} alt="" />
+                  </div>
+                  <div className={styles.infoName}>
+                    <span>Марина Путина</span>
+                    <div className={styles.infoDepartment}>СММ специалист</div>
+                  </div>
+                </div>
+                <img
+                  src={messageIcon}
+                  alt=""
+                  className={styles.messageUserIcon}
+                />
+              </div>
+
+              <div className={styles.personBox}>
+                <div className={styles.infoUser}>
+                  <div className={styles.imgUser}>
+                    <img src={miniUserIcon} alt="" />
+                  </div>
+                  <div className={styles.infoName}>
+                    <span>Марина Путина</span>
+                    <div className={styles.infoDepartment}>СММ специалист</div>
+                  </div>
+                </div>
+                <img
+                  src={messageIcon}
+                  alt=""
+                  className={styles.messageUserIcon}
+                />
+              </div>
+            </div>
+            <div className={styles.rightSide}></div>
+          </div>
+        </Container>
+      </div>
     </>
   );
 };
