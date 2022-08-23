2e973ab6f1d40ea26cc16de805c49977e158e589
ZAo Bao <tampooox@gmail.com>
2022-08-23 22:02:19 +0600

    prototype send messages

--- src/test.js
+++ src/test.js
@@ -1,11 +1,114 @@
-Array.prototype.sayLog = () => {
-    console.log(this);
+// {
+//   "rules": {
+//     "DBMessages": {
+//       	// ".read": "auth != null",
+//       	// ".write": "auth != null",
+//         "$uid2": {
+//           ".read": "$uid2 === auth.uid",
+//           "dialogues": {
+//             "$uid": {
+//               ".write": "$uid === auth.uid || $uid2 === auth.uid",
+//             }
+//           }
+//         }
+//     },
+//     "users": {
+//       ".read": true,
+//       "$uid": {
+//         ".write": "$uid === auth.uid",
+//       }
+//     }
+//   }
+// }
+
+
+// ПРОВЕРИТЬ ДАЗУ ДАННЫХ ЧТОБЫ ОНА БЫЛА ОДНА А НЕ 2
+
+const messages1 = { // Все исходящие сообщения пользователей
+    'uid1': { // СООБЩЕНИЯ КАКОГО ПОЛЬЗОВАТЕЛЯ [FROM:]
+        'uid3': [ // СООБЩЕНИЯ ДЛЯ КОГО [TO:]
+            {
+                date: '21.02.2022',
+                message: 'Привет'
+            },
+            {
+                date: '21.02.2022',
+                message: 'Как дела?'
+            },
+            {
+                date: '21.02.2022',
+                message: 'Классный день сегодня!'
+            }
+        ],
+        'uid4': [ // СООБЩЕНИЯ ДЛЯ КОГО [TO:]
+            {
+                date: '21.02.2022',
+                message: 'Привет'
+            },
+            {
+                date: '21.02.2022',
+                message: 'Как дела?'
+            },
+            {
+                date: '21.02.2022',
+                message: 'Классный день сегодня!'
+            }
+        ]
+    },
+    'uid2': { // СООБЩЕНИЯ КАКОГО ПОЛЬЗОВАТЕЛЯ [FROM:]
+        'uid1': [ // СООБЩЕНИЯ ДЛЯ КОГО [TO:]
+            {
+                date: '21.02.2022',
+                message: 'Привет'
+            },
+            {
+                date: '21.02.2022',
+                message: 'Как дела?'
+            },
+            {
+                date: '21.02.2022',
+                message: 'Классный день сегодня!'
+            }
+        ],
+        'uid3': [ // СООБЩЕНИЯ ДЛЯ КОГО [TO:]
+            {
+                date: '21.02.2022',
+                message: 'Привет'
+            },
+            {
+                date: '21.02.2022',
+                message: 'Как дела?'
+            },
+            {
+                date: '21.02.2022',
+                message: 'Классный день сегодня!'
+            }
+        ]
+    }
 }
 
-const arr = ["a", "b", "c"];
+const DATE = new Date()
 
-arr.sayLog()
+const DBMessages = {
+    "uid1": {
+        "dialogues": {
+            "uid3": {
+                "haveNewMessages": false,
+                "message": [{
+                        "date": DATE.toISOString(),
+                        "dateAt": DATE.getTime(),
+                        "text": "Привет!"
+                    },
+                    {
+                        "date": DATE.toISOString(),
+                        "dateAt": DATE.getTime(),
+                        "text": "Как дела?"
+                    }
+                ]
+            }
+        }
+    }
+}
 
-
-
-console.log(Array.forEach);
+console.log(new Date().toISOString());
+console.log(new Date().getTime());
--- src/test/TestPage.jsx
+++ src/test/TestPage.jsx
@@ -1,9 +1,56 @@
+//  ДЛЯ ТЕСТИРОВАНИЯ ИСПОЛЬЗОВАТЬ ТОЛЬКО ПОЛЬОВАТЕЛЯ:
+//
+//
+//               test@gg.gg
+//               test@gg.gg
+//
+//
+//
+//
+// Нормальные правила до ввода функции ЛИЧНЫЕ СООБЩЕНИЯ:
+//
+// {
+//   "rules": {
+//     "users": {
+//       ".read": true,
+//       "$uid": {
+//         ".write": "$uid === auth.uid"
+//       }
+//     }
+//   }
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
 import { useState, useEffect } from "react";
 
 // firebase
 import { auth } from "../firebase";
 import { db } from "../firebase";
-import { get, ref, set, onValue } from "firebase/database";
+import { get, ref, set, onValue, push, update } from "firebase/database";
 import {
   signInWithEmailAndPassword,
   updatePassword,
@@ -12,11 +59,21 @@ import {
 
 // Styles
 import styles from "./TestPage.module.scss";
+import { async } from "@firebase/util";
+
+const MY_UID = "Od7R2Epsz0QzDKdRp0Jie52CLOr2";
+const OTHER_USER_UID = "QPRtv6R3GndKDXNnk7JRc4yDEA83";
+
+const getSortMessage = (message) => message.sort((a, b) => a.dateAt - b.dateAt);
 
 const TestPage = () => {
   // State
   const [data, setData] = useState({ email: "", password: "" });
-  const [observable, setObservable] = useState({});
+  const [textMessage, setTextMessage] = useState("");
+  const [status, setStatus] = useState("не зашли");
+  const [myMessagesList, setMyMessagesList] = useState([]);
+  const [userMessagesList, setUserMessagesList] = useState([]);
+  const [allMessages, setAllMessages] = useState([]);
 
   // (f) setState
   const dataHandler = (e) => {
@@ -52,94 +109,220 @@ const TestPage = () => {
     }
   };
 
-  // (f) DB Запись - Send
+  // (f) Отправка сообщений
   const writeDBHandler = async (e) => {
     e.preventDefault();
 
-    const dataForSendDB = {
-      uuid: auth.currentUser.uid,
-      email: data.email,
-      password: data.password,
-    };
+    const DATE = new Date();
 
-    await set(ref(db, `/users/${auth.currentUser.uid}`), dataForSendDB);
+    // Отправить сообщение другому пользователю
+    async function sendNewMessageOtherUser(text) {
+      const refForNewMessage = push(
+        ref(db, `/DBMessages/${OTHER_USER_UID}/dialogues/${MY_UID}/message`)
+      );
+
+      await set(refForNewMessage, {
+        date: DATE.toISOString(),
+        dateAt: DATE.getTime(),
+        text: text,
+      });
+    }
+
+    // Отправить сообщение себе
+    async function sendNewMessageToYourself(text) {
+      const refForNewMessage = push(
+        ref(db, `/DBMessages/${MY_UID}/dialogues/${OTHER_USER_UID}/message`)
+      );
+
+      await set(refForNewMessage, {
+        from: auth.currentUser.uid,
+        date: DATE.toISOString(),
+        dateAt: DATE.getTime(),
+        text: text,
+      });
+    }
+
+    // Уведовить другово пользователя
+    async function changeFieldNotificationToTrue() {
+      const updates = {};
+      updates[
+        `/DBMessages/${OTHER_USER_UID}/dialogues/${MY_UID}/haveNewMessages`
+      ] = true;
+
+      await update(ref(db), updates);
+    }
+
+    // sendNewMessageOtherUser(textMessage);
+    sendNewMessageToYourself(textMessage);
+    // changeFieldNotificationToTrue();
+    setTextMessage("");
 
     console.log("Отправлено");
+  };
+
+  // (f) Обработчик сообщений
+  const handlerMessage = (e) => {
+    setTextMessage(e.target.value);
   };
 
   // (f) Чтение - Get
   const readDBHandler = async (e) => {
     e.preventDefault();
 
-    const response = await get(ref(db, `/users/${auth.currentUser.uid}`));
+    const response = await get(
+      ref(db, `/DBMessages/${OTHER_USER_UID}/dialogues/${MY_UID}`)
+    );
 
-    console.log(response.val());
+    // console.log(response && response.val());
+    Object.values(response.val().message).forEach((i) => console.log(i));
   };
 
-  // (e)
+  // (e) Установка стату "в сети" или "не в сети"
   useEffect(() => {
-    //
-    auth.onAuthStateChanged((us) => {
-      us && console.log("Зашли в учетную запись");
-      us || console.log("Вышли");
+    auth.onAuthStateChanged((user) => {
+      user && setStatus("в сети");
+      user || setStatus("не зашли");
     });
   }, []);
 
-  // (e) TEST TEST TEST
+  // (e) Загружаем сообщения с сервера 2 пользователей при первой загрузки
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
+    auth.onAuthStateChanged(async (user) => {
       if (user) {
-        onValue(ref(db, `/users/${auth.currentUser.uid}`), (snapshot) => {
-          const data = snapshot.val();
-          console.log(`snapshot: `, data);
-          setObservable(data);
+        const messageProcessing = (rawData) =>
+          Object.values(rawData.val().message);
+
+        const myMessages = new Promise((resolve, reject) => {
+          onValue(
+            ref(
+              db,
+              `/DBMessages/${auth.currentUser.uid}/dialogues/${OTHER_USER_UID}`
+            ),
+            (snapshot) => {
+              const dataMessage = messageProcessing(snapshot);
+              setMyMessagesList(dataMessage);
+              resolve(dataMessage);
+            }
+          );
         });
+
+        const userMessages = new Promise((resolve, reject) => {
+          onValue(
+            ref(
+              db,
+              `/DBMessages/${OTHER_USER_UID}/dialogues/${auth.currentUser.uid}`
+            ),
+            (snapshot) => {
+              const dataMessage = messageProcessing(snapshot);
+              setUserMessagesList(dataMessage);
+              resolve(dataMessage);
+            }
+          );
+        });
+
+        const response = await Promise.all([myMessages, userMessages]);
+
+        const messagesUnion = [...response[0], ...response[1]];
+
+        const sortMessage = getSortMessage(messagesUnion);
+
+        setAllMessages(sortMessage);
       }
     });
   }, []);
 
+  // (e) Получние новых сообщений реалтайм
+  useEffect(() => {
+    if (allMessages) {
+      setAllMessages(getSortMessage([...myMessagesList, ...userMessagesList]));
+    }
+  }, [myMessagesList, userMessagesList]);
+
   return (
     <div className={styles.mainTest}>
       <form action="/" onSubmit={handlerLogin}>
-        <div className={styles.buttonWrapper}>
-          <button onClick={exitHandler} type="button">
-            EXIT
-          </button>
-        </div>
-        <label>
-          <p>E-mail</p>
-          <input onChange={dataHandler} type="email" />
-        </label>
+        <div className={styles.login}>
+          <h1 className={styles.title}>Authorization</h1>
+          <div className={styles.btnGroup}>
+            <button
+              className={styles.btnExit}
+              onClick={exitHandler}
+              type="button"
+            >
+              EXIT
+            </button>
 
-        <label>
-          <p>Password</p>
-          <input onChange={dataHandler} type="password" />
-        </label>
+            <button className={styles.btnLogin} type="submit">
+              LOGIN
+            </button>
+          </div>
 
-        <div className={styles.buttonWrapper}>
-          <button type="submit">LOGIN</button>
-        </div>
+          <label>
+            <p>E-mail</p>
+            <input onChange={dataHandler} type="email" />
+          </label>
 
-        <div className={styles.buttonWrapper}>
-          <button onClick={writeDBHandler} type="button">
-            Send DB
-          </button>
-        </div>
-
-        <div className={styles.buttonWrapper}>
-          <button onClick={readDBHandler} type="button">
-            Get DB
-          </button>
+          <label>
+            <p>Password</p>
+            <input onChange={dataHandler} type="password" />
+          </label>
         </div>
 
-        <div>
-          <textarea
-            readOnly
-            value={JSON.stringify(observable)}
-            cols="35"
-            rows="7"
-            style={{ resize: "none" }}
-          />
+        <div className={styles.dataBase}>
+          <h1 className={styles.title}>Message</h1>
+
+          <span className={styles.titleUser}>
+            Вы:{" "}
+            <span
+              className={
+                status === "в сети"
+                  ? styles.titleUserStatusOnline
+                  : styles.titleUserStatusOffline
+              }
+            >
+              {status}
+            </span>
+          </span>
+
+          <div className={styles.dialog}>
+            <div className={styles.chat}>
+              {allMessages &&
+                allMessages.map((i) => {
+                  if (i.from === auth.currentUser.uid) {
+                    return (
+                      <div key={i.date} className={styles.rightUser}>
+                        <span className={styles.rightText}>{i.text}</span>
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div key={i.date} className={styles.leftUser}>
+                        <span className={styles.leftText}>{i.text}</span>
+                      </div>
+                    );
+                  }
+                })}
+            </div>
+            <div className={styles.inputBlock}>
+              <textarea
+                value={textMessage}
+                onChange={handlerMessage}
+                className={styles.inputText}
+              ></textarea>
+            </div>
+          </div>
+
+          <div className={styles.btnBD}>
+            <button onClick={writeDBHandler} type="button">
+              Send message
+            </button>
+          </div>
+
+          <div className={styles.btnBD}>
+            <button onClick={readDBHandler} type="button">
+              Get DB
+            </button>
+          </div>
         </div>
       </form>
     </div>
@@ -148,13 +331,24 @@ const TestPage = () => {
 
 export { TestPage };
 
-// {
-//   "rules": {
-//     "users": {
-//       "$uid": {
-//         ".read": "$uid === auth.uid",
-//         ".write": "$uid === auth.uid"
-//       }
-//     }
-//   }
-// }
+// const dataForSendDB = {
+//   [OTHER_USER_UID]: {
+//     dialogues: {
+//       [MY_UID]: {
+//         haveNewMessages: true,
+//         message: [
+//           {
+//             date: DATE.toISOString(),
+//             dateAt: DATE.getTime(),
+//             text: "Привет!",
+//           },
+//           {
+//             date: DATE.toISOString(),
+//             dateAt: DATE.getTime(),
+//             text: "Как дела?",
+//           },
+//         ],
+//       },
+//     },
+//   },
+// };
